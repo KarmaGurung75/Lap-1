cd2260b61aafa29ef516365e764c3fe2cc936f7b
Karma Gurung <63505288+KarmaGurung75@users.noreply.github.com>
2021-02-23 09:28:49 +0545

    lab_1

---
+++ print_sum.py
@@ -0,0 +1,8 @@
+#WAP that take three numbers and prints their sum.
+
+num1=int(input('Enter the first number'))
+num2=int(input('Enter the second number'))
+num3=int(input('Enter the third nmber'))
+
+sum=num1+num2+num3
+print("the sum of three numbers are",sum)
---
+++ valentine.py
@@ -0,0 +1,15 @@
+fun main (args: array<string> )
+{
+     if(today == "Valentine's day" && Me != "A programmer")
+{
+    date()
+    kisses()
+    other()
+}
+   else{
+    code()
+    sleep()
+    wakeup()
+    code again()
+   }
+}
