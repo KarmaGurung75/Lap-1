11c2a1d880818e10fbdc52378d26f56933ff2e55
Karma Gurung <63505288+KarmaGurung75@users.noreply.github.com>
2021-03-16 09:44:05 +0545

    lab_1

---
+++ Lab_1/convert_sec_to_day.py
@@ -0,0 +1,9 @@
+'''
+write a python program to convert second to day, hour,minutes and sec
+'''
+sec=int(input("Enter the seconds"))
+min=(sec//60)
+hrs=((sec//60)//60)
+day=(((sec//60)//60)//24)
+
+print(f"Day {day} Day {hrs} Hour {min} Mins {sec} sec")
---
+++ Lab_1/first_and_positive.py
@@ -0,0 +1,10 @@
+'''
+Write a python program to find sum of the first n positive integers
+'''
+'''n=int(input("Enter the number"))
+sum=(n*(n+1))/2
+print("The sum is ",sum)
+'''
+n=[1,2,3,4,5,6,7,8,9,10]
+total=sum(n)
+print(f"The sum is {total}")
